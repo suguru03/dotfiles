c5d4fd524110b262c4d23e8616b90f98a9b15035
Suguru Motegi <suguru.motegi@gmail.com>
2015-04-09 14:22:29 +0900

    add(brew): add brew setup file

---
+++ setup/brew.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+brew update
+brew upgrade
+
+brew tap homebrew/versions
+brew tap phinze/homebrew-cask
+brew tap homebrew/binary
+
+brew install subversion
+
+brew install vim
+
+brew install git
+brew install go
+brew install jq
+brew install nvm
+brew install rbenv
+brew install tig
+brew install tmux
+brew install zsh
+
+# Remove outdated versions
+brew cleanup
