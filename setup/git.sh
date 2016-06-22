5613a8905796bf87c94f86980d4d654de6411622
Suguru Motegi <suguru.motegi@gmail.com>
2016-06-22 16:41:26 -0700

    add git alias

--- setup/git.sh
+++ setup/git.sh
@@ -6,8 +6,12 @@ git config --global user.email "suguru.motegi@gmail.com"
 
 ## alias
 git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.fa "fetch --all"
+git config --global alias.cp "cherry-pick"
+git config --global alias.fo "fetch origin"
+git config --global alias.pop "stash pop"
 git config --global alias.soft "reset --soft"
 git config --global alias.hard "reset --hard"
-git config --global alias.fa "fetch --all"
-git config --global alias.fo "fetch origin"
-git config --global alias.cp "cherry-pick"
