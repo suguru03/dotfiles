73361665c76717c9c54a497909d6fd90caf45730
Suguru Motegi <suguru.motegi@gmail.com>
2017-04-16 17:16:21 -0700

    add ghq and peco config http://qiita.com/strsk/items/9151cef7e68f0746820d

--- setup/go.sh
+++ setup/go.sh
@@ -6,3 +6,5 @@ tar zxvf ~/Downloads/gcloud.tar.gz
 mv ~/Downloads/google-cloud-sdk ~/google-cloud-sdk
 ./google-cloud-sdk/install.sh
 
+go get github.com/motemen/ghq
+go get github.com/peco/peco/cmd/peco
