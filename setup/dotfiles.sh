f27e140846d4b1be1f86853e59d741384e0c6cc1
Suguru Motegi <suguru.motegi@gmail.com>
2018-01-04 09:54:47 -0800

    update powerline config

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/_vim ~/.vim
 ln -sh ~/dotfiles/_zshrc ~/.zshrc
 ln -sh ~/dotfiles/_gitconfig ~/.gitconfig
 ln -sh ~/dotfiles/_gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/_powerline-shell.json ~/.powerline-shell.json
