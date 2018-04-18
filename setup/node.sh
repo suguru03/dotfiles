0bbd598eff37e5c7c7ad78d849bcd311c068ee6a
Suguru Motegi <suguru.motegi@gmail.com>
2018-04-18 09:25:21 -0700

    clean up node setup file

--- setup/node.sh
+++ setup/node.sh
@@ -2,15 +2,8 @@
 
 npm install -g \
   eslint \
-  jshint \
-  babel-eslint \
-  eslint-plugin-react \
-  eslint-plugin-react-native \
-  eslint-plugin-prettier \
   react-native-cli \
   gulp \
-  bower \
-  mocha \
   pm2 \
   tern \
   tslint \
