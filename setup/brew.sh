15d57387b439781652f75e2e500974ae4447aba3
Suguru Motegi <suguru.motegi@gmail.com>
2022-06-18 17:50:29 -0700

    Update brew task

--- setup/brew.sh
+++ setup/brew.sh
@@ -5,7 +5,6 @@ brew update
 brew upgrade
 
 brew tap homebrew/versions || true
-brew tap phinze/homebrew-cask || true
 brew tap homebrew/binary || true
 
 brew install subversion
@@ -14,37 +13,15 @@ brew install vim
 
 brew install ag
 brew install git
-brew install go-app-engine-64
-brew install android-sdk
-brew install glide
 brew install jq
 brew install nvm
-brew install npm
 brew install rbenv
-brew install tig
 brew install tmux
 brew install zsh
 brew install wget
 brew install mysql
 brew install redis
 brew install coreutils
-brew install python
-brew linkapps python
-brew install cmake
-brew install ctags
-brew install asdf
-
-# docker
-brew install docker-machine-driver-xhyve
-
-# react native
-brew install watchman
-
-brew install caskroom/cask/brew-cask
-brew cask install virtualbox
-brew cask install vagrant
-
-brew install neovim/neovim/neovim
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
