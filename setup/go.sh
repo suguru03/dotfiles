3bf5146a3b89e9c6ccf28c41908a4c15146502f9
Suguru Motegi <suguru.motegi@gmail.com>
2022-06-18 18:02:55 -0700

    Clean up go and zshrc

--- setup/go.sh
+++ setup/go.sh
@@ -1,12 +1,6 @@
 #! /bin/sh
 
-git clone https://github.com/riywo/anyenv ~/.anyenv
-
-# gcloud
-curl curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-114.0.0-darwin-x86_64.tar.gz > ~/Downloads/gcloud.tar.gz
-tar zxvf ~/Downloads/gcloud.tar.gz
-mv ~/Downloads/google-cloud-sdk ~/google-cloud-sdk
-./google-cloud-sdk/install.sh
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 
 go get github.com/motemen/ghq
 go get github.com/peco/peco/cmd/peco
