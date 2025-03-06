93b7da51aedf6064a5676225a6679cc2e2dfecf4
Suguru Motegi <suguru.motegi@gmail.com>
2025-03-05 18:22:33 -0800

    Update karabiner config

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/_zshrc ~/.zshrc
 ln -sf ~/dotfiles/_gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/_gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/_powerline-shell.json ~/.powerline-shell.json
+ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner//karabiner.json
